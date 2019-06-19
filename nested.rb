7f8e14b9f476212ac7c023e50acd841b2aeaf4a7
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-19 07:30:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,9 +15,11 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
+
+hopper
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,7 +39,11 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]   
+  
 end
+
+alan_kay_is_known_for
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,6 +61,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,7 +86,13 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto] = {
+    known_for: "Ruby",
+    languages:
+      ["LISP", "C"]
+    }
+    
+  programmer_hash
     
 end
 
@@ -121,5 +134,10 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+ programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 
+end
+
+adding_to_dennis
 end
